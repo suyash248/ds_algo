13a04e86136d607de6bb0e4a742fe7b575f3360d
Suyash <suyash.soni248@gmail.com>
2016-05-13 18:05:51 +0530

    Init DS

---
+++ DataStructure/src/main/java/com/soni/ds/App.java
@@ -0,0 +1,13 @@
+package com.soni.ds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
