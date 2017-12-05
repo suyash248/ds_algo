557a6dc042b3c75dc129023b5038dde0aef1915c
suyash.soni <suyash.soni@srijans-MacBook-Pro.local>
2017-12-05 16:56:45 +0530

    height

---
+++ Tree/height.py
@@ -0,0 +1,31 @@
+from commons.commons import insert, print_tree, is_leaf
+
+def height(root):
+    if root == None:
+        return 0
+    return max(height(root.left), height(root.right)) + 1
+
+# Driver program to test above function
+if __name__ == "__main__":
+    """ Let us create following BST
+            50
+         /		 \
+        30		  70
+       /  \     / 	 \
+      20   40  60 	  80
+     /   \
+    15   25
+    """
+    root = None
+    root = insert(root, 50)
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 15);
+    insert(root, 25);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    h = height(root)
+    print "Height is {height}".format(height=h)
