974b6496c7f7c8650cd94c0691c689ae856c15c2
Suyash Soni <suyash.soni248@gmail.com>
2017-12-12 17:42:41 +0530

    Pairs with given sum in sorted array

---
+++ Array/pairWithSumInSortedArray.py
@@ -0,0 +1,22 @@
+def find_pair(arr, sum):
+    start = 0
+    end = len(arr)-1
+    pairs = []
+    while start < end:
+        curr_sum = arr[start] + arr[end]
+        if curr_sum < sum:
+            start += 1
+        elif curr_sum > sum:
+            end -= 1
+        else:
+            #print "({}, {})".format(arr[start], arr[end])
+            pairs.append((arr[start], arr[end]))
+            start += 1
+            end -= 1
+    return pairs
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    sum = 7
+    pairs = find_pair(arr, sum)
+    print "Pair(s) with sum equals to {sum} are {pairs}".format(sum=sum, pairs=pairs)
