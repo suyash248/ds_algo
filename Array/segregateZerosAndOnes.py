9c7bd71a49fd994edc3cbf3cc605e512ee45130b
Suyash Soni <suyash.soni248@gmail.com>
2017-12-13 11:21:17 +0530

    Segregate 0's and 1's V2

--- Array/segregateZerosAndOnes.py
+++ Array/segregateZerosAndOnes.py
@@ -1,7 +1,7 @@
-from Array import swap, print_array
+from Array import swap
 
 # O(n)
-def rearrange_v1(arr):
+def segregate_v1(arr):
     """
     Similar to quick sort partition by considering 0 as pivot.
     :param arr:
@@ -14,10 +14,33 @@ def rearrange_v1(arr):
             swap(arr, i, ip)
     return ip
 
+def segregate_v2(arr):
+    start = 0; end = len(arr) -1
+    mid = (start + end)/2
+
+    while start < end:
+        if arr[mid] == 0:
+            swap(arr, mid, start)
+            start += 1
+        elif arr[mid] == 1:
+            swap(arr, mid, end)
+            end -= 1
+
+
+# O(log(n))
 if __name__ == "__main__":
+    from copy import deepcopy
+
     arr = [1, 0, 1, 1, 0, 0, 1, 0]
-    pivot_index = rearrange_v1(arr)
+    arr_copy = deepcopy(arr)
+
+    print "\n ---- Using V1 ---- \n"
+    pivot_index = segregate_v1(arr)
     zeros_count = pivot_index + 1
     ones_count = len(arr) - zeros_count
     print "0's -> {} & 1's -> {}".format(zeros_count, ones_count)
     print "Rearranged array: ", arr
+
+    print "\n ---- Using V2 ---- \n"
+    segregate_v2(arr_copy)
+    print "Rearranged array: ", arr_copy
