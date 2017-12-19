c41cf53c5793d2449d69997362ac3ae4187d8c37
Suyash Soni <suyash.soni248@gmail.com>
2017-12-19 12:14:18 +0530

    Check if array can be partitioned into two subsets such that the sum of elements in both subsets is same

--- DynamicProgramming/equalSumSubsets.py
+++ DynamicProgramming/equalSumSubsets.py
@@ -1,6 +1,25 @@
 # Time complexity: O(2^n)
 # Space complexity: O(n)
 def is_subset_sum(arr, n, half_sum):
+    """
+    Algorithm ->
+
+    Case 1: If sum of all the elements in array is odd, array can't be partitioned into 2 subsets of equals sum.
+            So return `False`.
+
+    Case 2: If sum of all the elements in array is even then,
+        The is_subset_sum problem can be divided into two sub-problems
+         a) is_subset_sum() without considering last element. i.e. (reducing n to n-1)
+         b) is_subset_sum() considering the last element. i.e. (reducing n to n-1 and half_sum by arr[n-1])
+
+        If any of the above the above sub-problems return true, then return true.
+        isSubsetSum (arr, n, half_sum) = isSubsetSum (arr, n-1, half_sum) || isSubsetSum (arr, n-1, half_sum - arr[n-1])
+
+    :param arr: Input array
+    :param n: Array size, i.e. number of elements
+    :param half_sum: sum/2
+    :return:
+    """
     if half_sum == 0:
         return True
 
@@ -8,6 +27,7 @@ def is_subset_sum(arr, n, half_sum):
         return False
 
     return is_subset_sum(arr, n-1, half_sum) or is_subset_sum(arr, n-1, half_sum-arr[n-1])
+
 
 
 def is_sum_even(arr):
