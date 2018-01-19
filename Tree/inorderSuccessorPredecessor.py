3db7e99c9baa940d6e2c0750540209f598105fb0
Suyash Soni <suyash.soni248@gmail.com>
2018-01-19 21:03:11 +0530

    Fixed issues in Trees sections

--- Tree/inorderSuccessorPredecessor.py
+++ Tree/inorderSuccessorPredecessor.py
@@ -2,6 +2,24 @@ from commons.commons import insert, print_tree, is_leaf
 
 # This function finds predecessor and successor of key in BST
 # It sets pre and suc as predecessor and successor respectively
+"""
+1. If root is NULL
+      then return
+2. if key is found then
+    a. If its left subtree is not null
+        Then predecessor will be the right most 
+        child of left subtree or left child itself.
+    b. If its right subtree is not null
+        The successor will be the left most child 
+        of right subtree or right child itself.
+    return
+3. If key is smaller then root node
+        set the successor as root
+        search recursively into left subtree
+    else
+        set the predecessor as root
+        search recursively into right subtree
+"""
 def findPreSuc(root, key):
     # Base Case
     if root is None:
