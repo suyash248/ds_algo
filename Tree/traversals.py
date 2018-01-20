7c863d150faf0a0b3c3f884eecee146bb890de34
Suyash Soni <suyash.soni248@gmail.com>
2018-01-21 05:18:30 +0530

    Level order traversal

--- Tree/traversals.py
+++ Tree/traversals.py
@@ -18,15 +18,24 @@ def postorder(root):
         postorder(root.right)
         print root.key,
 
+def level_order(root):
+    from Queue import Queue
+    q = Queue()
+    q.put(root)
+    while not q.empty():
+        popped_elt = q.get()
+        print popped_elt.key,
+        if popped_elt.left: q.put(popped_elt.left)
+        if popped_elt.right: q.put(popped_elt.right)
 
 # Driver program to test above function
 if __name__ == "__main__":
     """ Let us create following BST
             50
          /		\
-        30		70
-        / 	\   / 	\
-        20 	40	60 	80
+        30		 70
+      /    \   / 	\
+    20 	   40 60 	80
     """
     root = None
     root = insert(root, 50)
@@ -37,6 +46,7 @@ if __name__ == "__main__":
     insert(root, 60)
     insert(root, 80)
 
-    print "Inorder is - ", inorder(root)
-    print "Preorder is - ", preorder(root)
-    print "Postorder is - ", postorder(root)
+    print "In-Order is - ", inorder(root)
+    print "Pre-Order is - ", preorder(root)
+    print "Post-Order is - ", postorder(root)
+    print "Level-Order is - ", level_order(root)
