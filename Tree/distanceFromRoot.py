04578d9f7af11519805fbc3525f6b54e1861f45e
Suyash Soni <suyash.soni248@gmail.com>
2018-02-25 03:22:22 +0530

    distance from root, version 3

--- Tree/distanceFromRoot.py
+++ Tree/distanceFromRoot.py
@@ -9,14 +9,13 @@ def distance_from_root_v1(root, key, distance=0):
     :return:
     """
     if root == None:
-        return 0
-    distance += 1
+        return -1
 
     if root.key == key:
         return distance
     else:
-        ld = distance_from_root_v1(root.left, key, distance)
-        rd = distance_from_root_v1(root.right, key, distance)
+        ld = distance_from_root_v1(root.left, key, distance+1)
+        rd = distance_from_root_v1(root.right, key, distance+1)
     return ld if ld > 0 else rd
 
 def distance_from_root_v2(root, key, level=0):
@@ -43,6 +42,18 @@ def distance_from_root_v2(root, key, level=0):
         right = distance_from_root_v2(root.right, key, level+1)
         return right
 
+
+def distance_from_root_v3(root, key, distance=0):
+    if root is None:
+        return -1
+
+    if key > root.key:
+        return distance_from_root_v3(root.right, key, distance+1)
+    elif key < root.key:
+        return distance_from_root_v3(root.left, key, distance+1)
+    else:
+        return distance
+
 # Driver program to test above function
 if __name__ == "__main__":
     """ Let us create following BST
@@ -66,10 +77,14 @@ if __name__ == "__main__":
     insert(root, 80)
 
     print "\n---- Using V1 ----\n"
-    key = 60
+    key = 25
     distance = distance_from_root_v1(root, key)
     print "Distance from root({root}) to {key} is {distance}".format(root=root.key, key=key, distance=distance)
 
     print "\n---- Using V2 ----\n"
-    distance_v2 = distance_from_root_v2(root, key, level=1)
+    distance_v2 = distance_from_root_v2(root, key, level=0)
     print "Distance from root({root}) to {key} is {distance}".format(root=root.key, key=key, distance=distance_v2)
+
+    print "\n---- Using V3 ----\n"
+    distance_v3 = distance_from_root_v3(root, key)
+    print "Distance from root({root}) to {key} is {distance}".format(root=root.key, key=key, distance=distance_v3)
