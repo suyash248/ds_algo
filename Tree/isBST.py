3355ffb1b19b4ef2b313ca3946c419608c6e01b7
Suyash Soni <suyash.soni248@gmail.com>
2018-02-25 01:40:28 +0530

    To check if binary tree is BST, version-2

--- Tree/isBST.py
+++ Tree/isBST.py
@@ -1,5 +1,6 @@
 import sys
-from commons.commons import insert
+from commons.commons import insert, is_leaf
+from Array import MAX, MIN
 
 def is_bst(root, min, max):
     if root is None:
@@ -9,6 +10,20 @@ def is_bst(root, min, max):
             (root.key >= min and root.key < max)
             and is_bst(root.left, min, root.key-1)
             and is_bst(root.right, root.key + 1, max)
+    )
+
+
+def is_bst_v2(root):
+    if root is None or is_leaf(root):
+        return True
+
+    left = root.left.key if root.left is not None else MIN
+    right = root.right.key if root.right is not None else MAX
+
+    return (
+            (root.key >= left and root.key < right)
+            and is_bst_v2(root.left)
+            and is_bst_v2(root.right)
     )
 
 # Driver program to test above function
@@ -56,10 +71,16 @@ if __name__ == "__main__":
         )
     )
 
-    MAX = sys.maxint
-    MIN = 1 - MAX
+    print "\n ---------- USING V1 ---------- \n"
+
     b1 = is_bst(root1, MIN, MAX)
     b2 = is_bst(root2, MIN, MAX)
+    print "Tree-1 is " + ("BST" if b1 else "Not BST")
+    print "Tree-2 is " + ("BST" if b2 else "Not BST")
 
+    print "\n ---------- USING V2 ---------- \n"
+
+    b1 = is_bst_v2(root1)
+    b2 = is_bst_v2(root2)
     print "Tree-1 is " + ("BST" if b1 else "Not BST")
     print "Tree-2 is " + ("BST" if b2 else "Not BST")
