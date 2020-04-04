805f1af1d73be197bb00cb844ebdd02dc453aa83
Suyash Soni <suyash.soni248@gmail.com>
2020-04-04 23:01:52 +0530

    detect cycle in undirected graph using DFS

--- Graph/detect_cycle_undirected_graph.py
+++ Graph/detect_cycle_undirected_graph.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 __author__ = "Suyash Soni"
 __email__ = "suyash.soni248@gmail.com"
 
-from typing import Tuple, TypeVar, Dict
+from typing import Tuple, TypeVar, Dict, Set
 from Graph.graph import Graph, Vertex, Edge
 from Graph.disjoint_set import DisjointSet, Node
 
@@ -31,9 +31,28 @@ def detect_cycle_using_disjoint_set(graph: Graph[T]) -> bool:
 
     return False
 
-# TODO
+# https://www.youtube.com/watch?v=eCG3T1m7rFY
 def detect_cycle_using_DFS(graph: Graph[T]) -> bool:
-    pass
+    all_data_vertex_mapping: Dict[T, Vertex[T]] = dict(graph.get_all_vertices())
+    visited: Set[T] = set()
+
+    def __detect_cycle__(vertex: Vertex[T], parent: Vertex[T], visited: Set[T]):
+        visited.add(vertex.data)
+        for adjacent_ver in vertex.get_all_adjacent_vertices():
+            if adjacent_ver.data not in visited:
+                if __detect_cycle__(adjacent_ver, vertex, visited):
+                    return True
+            # adjacent_ver is visited through some other node(ancestor) than it's parent, then there is some other
+            # path aslo to the adjacent_ver and that path doesn't contain it's parent. It means there is a cycle.
+            elif adjacent_ver != parent:
+                return True
+        return False
+
+    for data, ver in all_data_vertex_mapping.items():
+        if data not in visited:
+            if __detect_cycle__(ver, None, visited):
+                return True
+    return False
 
 # TODO
 def detect_cycle_using_BFS(graph: Graph[T]) -> bool:
@@ -44,7 +63,7 @@ if __name__ == '__main__':
     graph1: Graph[str] = Graph()
     graph1.add_edge('A', 'B', reverse=False)
     graph1.add_edge('B', 'C', reverse=False)
-    graph1.add_edge('C', 'A', reverse=False)
+    graph1.add_edge('C', 'D', reverse=False)
 
     print('Graph:')
     print(graph1, '\n')
@@ -56,7 +75,10 @@ if __name__ == '__main__':
     print("Undirected/unique edges:", graph1.get_all_undirected_edges())
 
     has_cycle = detect_cycle_using_disjoint_set(graph1)
-    print("\nGraph has cycle" if has_cycle else "Graph does NOT have cycle")
+    print("\nUsing Disjoint set - " + ("Graph has cycle" if has_cycle else "Graph does NOT have cycle"))
+
+    has_cycle = detect_cycle_using_DFS(graph1)
+    print("\nUsing DFS - " + ("Graph has cycle" if has_cycle else "Graph does NOT have cycle"))
 
     graph2: Graph[int] = Graph()
     graph2.add_edge(0, 1, reverse=False)
@@ -78,5 +100,8 @@ if __name__ == '__main__':
     print("Undirected/unique edges:", graph2.get_all_undirected_edges())
 
     has_cycle = detect_cycle_using_disjoint_set(graph2)
-    print("\nGraph has cycle" if has_cycle else "Graph does NOT have cycle")
+    print("\nUsing Disjoint set - " + ("Graph has cycle" if has_cycle else "Graph does NOT have cycle"))
+
+    has_cycle = detect_cycle_using_DFS(graph2)
+    print("\nUsing DFS - " + ("Graph has cycle" if has_cycle else "Graph does NOT have cycle"))
 
