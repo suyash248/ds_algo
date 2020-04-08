0ff326f0053674d5d752e509b95f4af5db41d67e
Suyash Soni <suyash.soni248@gmail.com>
2020-04-09 02:47:08 +0530

    prims using dict

--- Graph/mst/prims_mst.py
+++ Graph/mst/prims_mst.py
@@ -13,17 +13,19 @@ K = TypeVar('K')
 
 # https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/PrimMST.java
 # https://www.youtube.com/watch?v=oP2-8ysT3QQ
+# Space complexity: O(E+V)
+# Time complexity: O(E*log(V))
 def prims_mst(graph: Graph[T]) -> List[Edge[T]]:
+    # Vertex to min edge weight mapping. this is used to pick the vertex having a connecting edge with min. weight by
+    # performing extract_min() call.
     # Each element/node(HeapNode) in the heap contains Vertex as data and edge weight.
     min_binary_heap: MinBinaryHeap[Vertex[T], int] = MinBinaryHeap()
     vertex_edge_mapping: Dict[Vertex[T], Edge[T]] = dict()
     adjacency_list: Dict[Vertex[T], Set[Edge[T]]] = graph.adjacency_list
-    # start_vertex: Vertex[T] = None
 
     for i, (data, vertex) in enumerate(graph.vertices):
         if i == 0:
-            # Starting from the `start_vertex` and making weight corresponding to it as 0, so that extract_min() will
-            # return `start_vertex` from the heap.
+            # Make weight of one node 0 so that this vertex is picked as first vertex .
             min_binary_heap.push(vertex, 0)
         else:
             min_binary_heap.push(vertex, sys.maxsize)
@@ -31,16 +33,59 @@ def prims_mst(graph: Graph[T]) -> List[Edge[T]]:
     while min_binary_heap.size > 0:
         popped_heap_node: HeapNode[Vertex[T], int] = min_binary_heap.extract_min()
         for connecting_edge in adjacency_list[popped_heap_node.data]:
-            # connecting_edge: vertex1 -> vertex2 (here vertex1 = min_vertex, vertex2 = connecting.edge.vertex2)
+            # connecting_edge: vertex1 -> vertex2 (vertex1 = popped_heap_node.data, vertex2 = connecting.edge.vertex2)
             heap_node: HeapNode[Vertex[T], int] = min_binary_heap.peek(connecting_edge.vertex2)
 
-            # If adjacent vertex(connecting_edge.vertex2) exists in min_heap and edge weight is lower
+            # If adjacent vertex(connecting_edge.vertex2) exists in min_heap and edge weight is lower. update the weight
+            # for `heap_node` in min_heap and add this edge to output(vertex_edge_mapping)
+            # against connecting_edge.vertex2.
             if heap_node and connecting_edge.weight < heap_node.weight:
                 min_binary_heap.replace_weight(heap_node.data, connecting_edge.weight)
 
                 # Update MST dict
                 vertex_edge_mapping[connecting_edge.vertex2] = connecting_edge
     return list(vertex_edge_mapping.values())
+
+
+# https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/?ref=rp
+# Time complexity: O(V*2)
+# Space complexity: O(E+V)
+def prims_mst_v2(graph: Graph[T]) -> List[Edge[T]]:
+    # stores vertex --> edge(with min weight mapping).
+    vertex_edge_mapping: Dict[Vertex[T], Edge[T]] = dict()
+    adjacency_list: Dict[Vertex[T], Set[Edge[T]]] = graph.adjacency_list
+
+    # Vertex to min edge weight mapping. this is used to pick the vertex having a connecting edge with min. weight.
+    vertex_edge_weight_mapping: Dict[Vertex[T], int] = dict()
+
+    for i, (data, vertex) in enumerate(graph.vertices):
+        if i == 0:
+            vertex_edge_weight_mapping[vertex] = 0
+        else:
+            vertex_edge_weight_mapping[vertex] = sys.maxsize
+
+    while len(vertex_edge_weight_mapping) > 0:
+        # Extract(get & pop) vertex with min edge weight from vertex_edge_weight_mapping dict.
+        vertex_min_wt_tuple: Tuple[Vertex[T], int] = (None, sys.maxsize)
+        for vertex, wt in vertex_edge_weight_mapping.items():
+            if wt < vertex_min_wt_tuple[1]:
+                vertex_min_wt_tuple = (vertex, wt)
+        vertex_with_min_edge_weight: Vertex[T] = vertex_min_wt_tuple[0]
+        vertex_edge_weight_mapping.pop(vertex_with_min_edge_weight)
+
+        for connecting_edge in adjacency_list[vertex_with_min_edge_weight]:
+            # connecting_edge: vertex1 -> vertex2 (here vertex1 = popped_heap_node.data, vertex2 = connecting.edge.vertex2)
+            adj_ver_weight: int = vertex_edge_weight_mapping.get(connecting_edge.vertex2)
+
+            # If adjacent vertex(connecting_edge.vertex2) exists in vertex_edge_weight_mapping and edge weight is lower.
+            # update vertex_edge_weight_mapping and add this edge to output(vertex_edge_mapping)
+            # against connecting_edge.vertex2.
+            if connecting_edge.vertex2 in vertex_edge_weight_mapping and connecting_edge.weight < adj_ver_weight:
+                vertex_edge_weight_mapping[connecting_edge.vertex2] = connecting_edge.weight
+                vertex_edge_mapping[connecting_edge.vertex2] = connecting_edge
+
+    return list(vertex_edge_mapping.values())
+
 
 if __name__ == '__main__':
     #
@@ -64,7 +109,9 @@ if __name__ == '__main__':
     graph.add_edge('D', 'E', weight=6)
     graph.add_edge('E', 'F', weight=2)
 
+    print("Adjacency list:")
     print(graph)
+    print('\n' + '#' * 75 + '\n')
 
     mst_edges: List[Edge] = prims_mst(graph)
     total_weight: int = sum(map(lambda e: e.weight, mst_edges))
@@ -74,4 +121,17 @@ if __name__ == '__main__':
 
     # 9
     print("MST:", mst_edges)
+
+    print('\n' + '#' * 75 + '\n')
+
+    mst_edges: List[Edge] = prims_mst_v2(graph)
+    total_weight: int = sum(map(lambda e: e.weight, mst_edges))
+
+    # [C--(1)-->B, A--(1)-->D, F--(2)-->E, D--(1)-->C, C--(4)-->F]
+    print("\nTotal weight of MST:", total_weight)
+
+    # 9
+    print("MST:", mst_edges)
+
+
 
---
+++ util/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "Suyash Soni"
+__email__ = "suyash.soni248@gmail.com"
+
+from typing import Generic, TypeVar, Dict
