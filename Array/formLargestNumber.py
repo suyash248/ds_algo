47298f73d6ce5bea8182d330342ecf6e80c1c85c
suyash248 <suyash.soni248@gmaiil.com>
2020-09-14 05:38:29 +0530

    form largest number

--- Array/formLargestNumber.py
+++ Array/formLargestNumber.py
@@ -6,13 +6,15 @@ def compare(a, b):
     return str(m)
 
 def form_largest_number(arr):
+    arr.sort()
     arr = map(str, arr)
 
     largest_num = reduce(compare, arr)
     return largest_num
 
 if __name__ == "__main__":
-    arr = [54, 546, 548, 60]
+    arr = [250, 74, 659, 931, 273, 545, 879, 924] # 93192487974659545273250
+    # arr = [54, 546, 548, 60]
     #arr = [1, 34, 3, 98, 9, 76, 45, 4]
     largest_num = form_largest_number(arr)
     print ("Largest number :", largest_num)
